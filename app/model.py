60150538e0bb6d9b36311cf55886f0f5c4822a58
Antonio Rueda-Toicen <antonio.rueda.toicen@gmail.com>
2025-09-05 15:38:46 +0000

    update repo

--- app/model.py
+++ app/model.py
@@ -6,6 +6,12 @@ from loadotenv import load_env
 load_env()
 wandb_api_key = os.environ.get("WANDB_API_KEY")
 
+# This is the local folder where the wandb model will be downloaded
+MODELS_DIR = "../models"
+MODEL_FILENAME = "best_model.pth"
+
+os.makedirs(MODELS_DIR, exist_ok=True)
+
 def download_artifact():
     assert 'WANDB_API_KEY' in os.environ, "WANDB_API_KEY not found in environment variables"
     wandb.login(key=wandb_api_key)
@@ -17,5 +23,9 @@ def download_artifact():
     wandb_model_version = os.environ.get("WANDB_MODEL_VERSION")
     
     artifact_path = f'{wandb_org}/{wandb_project}/{wandb_model_name}:{wandb_model_version}'
+    print(f"Downloading artifact from {artifact_path}")
+    
+    artifact = api.artifact(artifact_path, type='model')
+    artifact.download(root=MODELS_DIR)
 
 download_artifact()
